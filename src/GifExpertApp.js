bced90ff6a73504c67e1132abfa6490fbfb7293a
Daniel Celis <bryan_dca@hotmail.com>
2022-05-07 13:19:41 -0600

    Cambios finales

---
+++ src/GifExpertApp.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+function GifExpertApp() {
+
+
+    const [categories, setCategories] = useState(['Deku']);
+    
+    
+  return (
+    <>
+        <h2>GifExpertApp</h2>
+
+        <AddCategory  setCategories={ setCategories }/>
+
+        <hr></hr>
+        
+        
+        <ol>
+            {
+                categories.map( category => (
+                    <GifGrid
+                        key={category}
+                        category={category}
+                    />
+                ))
+            }
+        </ol>
+    </>
+    
+  );
+}
+
+export default GifExpertApp;
---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react"
+import {getGifs} from '../helpers/getGifs'
+
+
+export const useFetchGifs = (category) => {
+
+    const [first, setFirst] = useState({
+        data: [],
+        loading: true
+    });
+
+
+    useEffect( () =>{
+        getGifs(category)
+                .then(imgs => {
+                    
+                        setFirst({
+                            data: imgs,
+                            loading: false
+                        })
+                })
+    }, [category])
+    
+    
+   
+
+    return first;
+}
